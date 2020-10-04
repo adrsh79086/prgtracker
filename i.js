2ce6e1032b923dedc1cce1972480bddea4e91cca
adrsh79086 <72243915+adrsh79086@users.noreply.github.com>
2020-10-04 21:37:51 +0530

    Add files via upload

---
+++ i.js
@@ -0,0 +1,11 @@
+$("#slider").roundSlider({
+    sliderType: "min-range",
+    handleShape: "dot",
+    width: 22,
+    radius: 100,
+    value: 95,
+    circleShape: "pie",
+    lineCap: "round",
+
+   
+});
